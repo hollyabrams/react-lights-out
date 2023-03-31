23ae47a53b402d9c9b6389d35878752a5232ab3c
Holly <holly.d.abrams@gmail.com>
2023-03-31 12:38:42 -0400

    Updated package.json to include homepage

--- src/Board.test.js
+++ src/Board.test.js
@@ -5,7 +5,7 @@ test('renders Board without crashing', () => {
     render(<Board nrows={5} ncols={5} chanceLightStartsOn={0.5} />);
 });
   
-  
+ 
 test('renders Board correctly', () => {
     const { container } = render(<Board nrows={5} ncols={5} chanceLightStartsOn={0.5} />);
     expect(container.firstChild).toMatchSnapshot();
