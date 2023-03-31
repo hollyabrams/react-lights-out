ac56ba6247fb8a217630fc43e07fcf3231ae4ba0
Holly <holly.d.abrams@gmail.com>
2023-03-31 12:16:43 -0400

    Added testing

---
+++ src/Board.test.js
@@ -0,0 +1,12 @@
+import { render } from '@testing-library/react';
+import Board from './Board';
+
+test('renders Board without crashing', () => {
+    render(<Board nrows={5} ncols={5} chanceLightStartsOn={0.5} />);
+});
+  
+  
+test('renders Board correctly', () => {
+    const { container } = render(<Board nrows={5} ncols={5} chanceLightStartsOn={0.5} />);
+    expect(container.firstChild).toMatchSnapshot();
+});
